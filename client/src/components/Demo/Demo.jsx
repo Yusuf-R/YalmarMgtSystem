6582ad5cd68ffdc714410c98e49e7bbd07a69ac1
Yusuf <y.abdulwasiu@gmail.com>
2024-10-25 12:06:27 +0100

    refactor: :recycle: removed redundant routes

--- client/src/components/Demo/Demo.jsx
+++ client/src/components/Demo/Demo.jsx
@@ -1,10 +1,12 @@
 'use client';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 function Demo () {
     return (
         <>
             <Box>
                 <Typography>
-                    Under Construction
+                    Under Construction !!!
                 </Typography>
             </Box>
         </>
